29db8e94f826b8ab09da7b63519ddba5ec3a8ee8
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-25 14:54:32 +0200

    add checker if input directory exist

--- VtoIMG.py
+++ VtoIMG.py
@@ -5,6 +5,9 @@ import sys
 
 video_ext = ['.mp4','.mkv','.mov','.avi','.wmv','webm']
 input_dir = 'input'
+
+if not os.path.exists(input_dir):
+
 for ext in video_ext:
     # make a list contain all of the file with specific extension
     video_files = [file for file in os.listdir(input_dir) if file.endswith(ext)]
