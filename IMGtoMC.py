6c1a5253eb356b6ec9d537857340b98c795b450a
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-26 13:25:19 +0200

    add images iterator

--- IMGtoMC.py
+++ IMGtoMC.py
@@ -11,3 +11,8 @@ if not os.path.exists(input_dir):
     print(f'Input Directory <{input_dir}> created')
     print(f'Please add Images into <{input_dir}> folder')
     sys.exit(1)
+
+for file in os.listdir(input_dir):
+    file_path = os.path.join(input, file)
+    if os.path.isfile(file_path):
+        print(f'File found : {file_path}')
