7ce9151f59992da82d66d9bc6dda6d7aafb5c414
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-27 09:20:59 +0200

    Update IMGtoMC.py

--- IMGtoMC.py
+++ IMGtoMC.py
@@ -12,7 +12,40 @@ def imgconvertor(file_path):
     global i
     
     image = cv2.imread(file_path)
+    width = 96
+    height = 54
+    # width = 64
+    # height = 98
+    resized_image = cv2.resize(image, (width,height))
 
+    # Loop through each pixel in the resized image
+    height, width, _ = resized_image.shape
+
+    with open(f'heaven{i}.mcfunction', 'w') as file:
+        # Redirect stdout to the file
+        sys.stdout = file
+
+        for y in range(height):
+            for x in range(width):
+                pixel = resized_image[y, x]
+
+                # Get color (BGR values)
+                blue, green, red = pixel
+
+                blue = round(float(blue) * (1/255),1)
+                green = round(float(green) * (1/255),1)
+                red = round(float(red) * (1/255),1)
+
+                # Calculate brightness
+                brightness = (float(blue) + float(green) + float(red)) / 3
+
+                # Print pixel information
+                # print(f"Pixel at ({x}, {y}) - Color: ({blue}, {green}, {red}), Brightness: {brightness}")
+                print(f'particle minecraft:dust {red} {green} {blue} 1 ~8 ~{((width-y)/6)-7} ~{((x)/6)-5}')
+
+    # Reset stdout to its default
+    sys.stdout = sys.__stdout__
+    i = i + 1
 
 # check if images directory doesnt exist and create one
 if not os.path.exists(images_dir):
