eabd8eb432b438fbd68661bb62509f6119d0b278
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-29 12:39:38 +0200

    Update mainCreator.py

--- mainCreator.py
+++ mainCreator.py
@@ -10,3 +10,10 @@ if not os.path.exists(functions_dir):
     print(f'there is no directory called <{functions_dir}')
     print(f'have you run the image-to-particle commands')
     sys.exit(1)
+
+# count how many files(function) in the folder
+file_count = len([file for file in os.listdir(functions_dir) if os.path.isfile(os.path.join(functions_dir, file))])
+if file_count == 0:
+    print(f'there is {file_count} in the folder')
+    print('!!force exit!!')
+    sys.exit(1)
