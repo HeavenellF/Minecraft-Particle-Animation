0268c1832bf0a552bea0335d10d78b6f5259e0f9
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-29 12:00:01 +0200

    Update mainCreator.py

--- mainCreator.py
+++ mainCreator.py
@@ -2,3 +2,11 @@ import os
 import sys
 
 
+functions_dir = 'wao'
+# functions directory is the directory where all the function located then
+
+# check if the functions directory doesnt exist and force exit
+if not os.path.exists(functions_dir):
+    print(f'there is no directory called <{functions_dir}')
+    print(f'have you run the image-to-particle commands')
+    sys.exit(1)
